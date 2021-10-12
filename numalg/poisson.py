55537447fe2e55dc3fa31217c80765b4aea47766
Alexander Johan Arntzen <alexander.arntzen@gmail.com>
2021-10-12 16:09:21 +0200

    fixed cg mayby

--- numalg/poisson.py
+++ numalg/poisson.py
@@ -1,38 +1,49 @@
 import numpy as np
 
 
-def L(u, index):
-    """Compute discrete laplacian times h**2"""
+def A(u, N):
+    """Compute negative discrete laplacian times h**2 + id on the boundaryt """
+    index = np.arange(1, N)
     ixy = np.ix_(index, index)
     ixm_y = np.ix_(index - 1, index)
     ixp_y = np.ix_(index + 1, index)
     ix_ym = np.ix_(index, index - 1)
     ix_yp = np.ix_(index, index + 1)
 
-    Lu = -4 * u[ixy] + u[ixm_y] + u[ixp_y] + u[ix_ym] + u[ix_yp]
+    # id for boundary points
+    Lu = u.copy()
+
+    # - \delta
+    Lu[ixy] = -(u[ixm_y] + u[ixp_y] + u[ix_ym] + u[ix_yp]) + 4 * u[ixy]
     return Lu
 
 
-def cg(u_0, rhs, N, tol):
-    """Solving Lu*h**2 = f*h**2
+def cg(A, u_0, rhs, N, tol):
+    """Solving -Lu*h**2 = f*h**2*1(interior) + g*boundary()
 
-    rhs must f*h**2"""
-    index = np.arange(1, N)
-    u = u_0
-    r = rhs - L(u, index)
+    Assuming all numbers are real
+    """
+
+    r = rhs - A(u_0, N)
     p = r
-    r_dot_r = np.tensordot(r, r, 2)
-    while True:
-        Lp = L(p, index)
-        alpha = r_dot_r / np.tensordot(Lp, p)
+    u = u_0
+    r_dot_r_0 = np.tensordot(r, r, 2)  # two norm **2
+    r_dot_r = r_dot_r_0
+    i = 0
+    while r_dot_r / r_dot_r_0 >= tol ** 2:
+        # if i % 100 == 0:
+        #     print(i, ((r_dot_r / r_dot_r_0) ** 0.5))
 
-        u = u - alpha * p
-        r = r - alpha * Lp
+        Ap = A(p, N)
+        alpha = r_dot_r / np.tensordot(Ap, p, 2)
+
+        u = u + alpha * p
+        r = r - alpha * Ap
         r_dot_r_new = np.tensordot(r, r, 2)
-        beta = r_dot_r / r_dot_r_new
+        beta = r_dot_r_new / r_dot_r
         p = r + beta * p
 
+        # update for iteration
         r_dot_r = r_dot_r_new
-
-        break
-    return u_0
+        i += 1
+    return u
--- test/test_poisson.py
+++ test/test_poisson.py
@@ -1,6 +1,9 @@
 import unittest
 import numpy as np
-from numalg.poisson import cg
+
+from scipy.sparse.linalg import LinearOperator
+import scipy.sparse.linalg as lin
+from numalg.poisson import cg, A
 
 
 class TestCase1(unittest.TestCase):
@@ -10,7 +13,7 @@ class TestCase1(unittest.TestCase):
         y = np.linspace(0, 1, N + 1)
         X, Y = np.meshgrid(x, y, indexing="ij")
 
-        f = 20 * np.pi ** 2 * np.sin(2 * np.pi * X) * np.sin(2 * np.pi * Y)
+        f = 20 * np.pi ** 2 * np.sin(2 * np.pi * X) * np.sin(4 * np.pi * Y)
         return f
 
     @staticmethod
@@ -19,18 +22,29 @@ class TestCase1(unittest.TestCase):
         y = np.linspace(0, 1, N + 1)
         X, Y = np.meshgrid(x, y, indexing="ij")
 
-        U = np.sin(2 * np.pi * X) * np.sin(2 * np.pi * Y)
+        U = np.sin(2 * np.pi * X) * np.sin(4 * np.pi * Y)
         return U
+
+    def get_rhs(self, N):
+        # assuming g=U on the boundary, which is true
+
+        ix = np.arange(1, N)
+        ixy = np.ix_(ix, ix)
+
+        rhs = self.get_u(N)  # = g
+        rhs[ixy] = self.get_f(N)[ixy] / N ** 2
+
+        return rhs
 
     def test_cg(self):
         for N in [32, 64, 128]:
-            rhs = self.get_f(N)
+
+            rhs = self.get_rhs(N)
             U_sol = self.get_u(N)
             U_0 = np.random.rand(N + 1, N + 1)
-
-            U_num = cg(U_0, rhs=rhs, N=N, tol=10e-12)
-
-            np.testing.assert_allclose(U_num, U_sol)
+            U_num = cg(A, U_0, rhs=rhs, N=N, tol=2 / N ** 1.5)
+            DU = U_num - U_sol
+            self.assertAlmostEqual(np.max(np.abs(DU)), 0, delta=0.1)
 
 
 if __name__ == "__main__":
