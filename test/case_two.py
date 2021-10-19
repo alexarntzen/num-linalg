7e4b3e95699e26a6f3c83e844991e63027112df2
Alexander Johan Arntzen <alexander.arntzen@gmail.com>
2021-10-19 16:18:06 +0200

    implemented residual test and test case 2

---
+++ test/case_two.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+def get_f(N):
+    x = np.linspace(0, 1, N + 1)
+    y = np.linspace(0, 1, N + 1)
+    X, Y = np.meshgrid(x, y, indexing="ij")
+
+    f = X * 0 - 1  # hack to get correct shape
+    return f
+
+
+def get_g(N):
+    x = np.linspace(0, 1, N + 1)
+    y = np.linspace(0, 1, N + 1)
+    X, Y = np.meshgrid(x, y, indexing="ij")
+
+    g = X*0
+
+    ix = np.arange(1)
+    iy = np.arange(0, N+1)
+    ixy = np.ix_(ix, iy)
+    g[ixy] = 4*Y[ixy]*(1-Y[ixy])
+
+    return g
+
+
+# get_u = get_g
+
+def get_rhs(N):
+    # assuming g=U on the boundary, which is true
+
+    ix = np.arange(1, N)
+    ixy = np.ix_(ix, ix)
+
+    rhs = get_g(N) # = g
+    rhs[ixy] = get_f(N)[ixy] / N ** 2
+
+    return rhs
+
+
+def get_u_0(N):
+    return np.random.rand(N + 1, N + 1)
--- test/test_iterative.py
+++ test/test_iterative.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 
 import test.case_one as c1
+import test.case_two as c2
 
 from numalg.laplacian import L, D_inv, J_w
 from numalg.iterative import cg, weighted_jacobi
@@ -46,6 +47,18 @@ class TestIterCaseOne(unittest.TestCase):
             DU = U_num - U_sol
             print(f"detla_{N}: {np.max(np.abs(DU))}")
             self.assertAlmostEqual(np.max(np.abs(DU)), 0, delta=0.1)
+
+class TestResdiual(unittest.TestCase):
+    def test_multigrid(self):
+        print("\nMultigrid:")
+        for case in [c1, c2]:
+            for N in [32, 64,128]:
+                rhs = case.get_rhs(N)
+                U_0 = case.get_u_0(N)
+                U_num = mgv_poisson(U_0, rhs, N, nu1=2000, nu2=2000, level=0, max_level=2)
+                res = L(U_num,N) - rhs
+                print(f"Residual_{N}: {np.max(np.abs(res))}")
+                self.assertAlmostEqual(np.max(np.abs(res)), 0, delta=1e-3)
 
 
 if __name__ == "__main__":
