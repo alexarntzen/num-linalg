26e91752804135a1dd5ba19935629a1ab3e7c24a
Alexander Johan Arntzen <alexander.arntzen@gmail.com>
2021-11-09 11:42:52 +0100

    implemented and tested second case

--- test/case_matrix_ode.py
+++ test/case_matrix_ode.py
@@ -2,7 +2,92 @@
 import scipy.linalg as sl
 import numpy as np
 
-laplace_kernel_1d = np.array([1, -2, -1])
+
+def generate_heat_equation(n, m, k):
+    dxx, dyy = get_laplacians(m, n)
+    D = kron_sum(dxx, dyy)
+    B = D
+    C_0 = np.random.rand(m, k)
+    D_0 = np.random.rand(m, k)
+
+    A_0 = C_0 @ D_0.T
+
+    def A(t) -> np.ndarray:
+        """since exp(N (+) M) =  exp(N) (x) exp(M)"""
+        expXt = sl.expm(dxx * t)
+        expYt = sl.expm(dyy * t)
+        expBt = np.kron(expXt, expYt)
+        return raveldot(expBt, A_0)
+
+    def A_dot(t) -> np.ndarray:
+        A_dot = raveldot(B, A(t))
+        return A_dot
+
+    return A_0, A, A_dot
+
+
+def generate_first_example(eps=1e-3):
+    n = 100
+    T_1 = get_skew_symmetric(n, [1])
+    T_2 = get_skew_symmetric(n, [0.5, 1])
+    A_1 = get_A(eps=eps)
+    A_2 = get_A(eps=eps)
+
+    A_0 = A_1 + A_2
+
+    def Q_1(t):
+        return sl.expm(T_1 * t)
+
+    def Q_2(t):
+        return sl.expm(T_2 * t)
+
+    def A(t):
+        return Q_1(t) @ (A_1 + np.exp(t) * A_2) @ Q_2(t).T
+
+    def A_dot(t):
+        Q_1_t = Q_1(t)
+        Q_2_t = Q_2(t)
+        A_t = A(t)
+
+        d_one = T_1 @ A_t
+        d_two = A_t @ T_2.T
+        d_tree = Q_1_t @ (np.exp(t) * A_2) @ Q_2_t.T
+
+        return d_one + d_two + d_tree
+
+    return A_0, A, A_dot
+
+
+def generate_second_example(eps=1e-1):
+    n = 100
+    T_1 = get_skew_symmetric(n, [1])
+    T_2 = get_skew_symmetric(n, [0.5, 1])
+    A_1 = get_A(eps=eps)
+    A_2 = get_A(eps=eps)
+
+    A_0 = A_1 + A_2
+
+    def Q_1(t):
+        return sl.expm(T_1 * t)
+
+    def Q_2(t):
+        return sl.expm(T_2 * t)
+
+    def A(t):
+        return Q_1(t) @ (A_1 + np.cos(t) * A_2) @ Q_2(t).T
+
+    def A_dot(t):
+        Q_1_t = Q_1(t)
+        Q_2_t = Q_2(t)
+        A_t = A(t)
+
+        d_one = T_1 @ A_t
+        d_two = A_t @ T_2.T
+        d_tree = Q_1_t @ (-np.sin(t) * A_2) @ Q_2_t.T
+
+        return d_one + d_two + d_tree
+
+    return A_0, A, A_dot
 
 
 def raveldot(A: np.ndarray, x: np.ndarray):
@@ -18,6 +103,7 @@ def get_laplacians(m, n):
     assert n > 2 and m > 2, "cannot make laplacian with so few nodes"
     # dxx = sl.convolution_matrix(laplace_kernel_1d * m ** 2, m, mode="same")
     # dyy = sl.convolution_matrix(laplace_kernel_1d * n ** 2, n, mode="same")
+    laplace_kernel_1d = np.array([1, -2, -1])
     dxx = sl.convolution_matrix(laplace_kernel_1d, m, mode="same")
     dyy = sl.convolution_matrix(laplace_kernel_1d, n, mode="same")
     return dxx, dyy
@@ -31,88 +117,19 @@ def kron_sum(A, B):
     return left + right
 
 
-class HeatEquation:
-    """d/dt A(t)= D @ A(t)"""
+def get_skew_symmetric(n, coeffs):
+    T = np.zeros((n, n))
+    for k, coeff in enumerate(coeffs):
+        i = k + 1
+        np.fill_diagonal(T[i:, :-i], coeff)
+        np.fill_diagonal(T[:-i, i:], -coeff)
 
-    @staticmethod
-    def generate_case(n, m, k):
-        dxx, dyy = get_laplacians(m, n)
-        D = kron_sum(dxx, dyy)
-        B = D
-        C_0 = np.random.rand(m, k)
-        D_0 = np.random.rand(m, k)
-
-        A_0 = C_0 @ D_0.T
-
-        def A(t) -> np.ndarray:
-            """since exp(N (+) M) =  exp(N) (x) exp(M)"""
-            expXt = sl.expm(dxx * t)
-            expYt = sl.expm(dyy * t)
-            expBt = np.kron(expXt, expYt)
-            return raveldot(expBt, A_0)
-
-        def A_dot(t) -> np.ndarray:
-            A_dot = raveldot(B, A(t))
-            return A_dot
-
-        return A_0, A, A_dot
+    return T
 
 
-class FirstExample:
-    """d/dt A(t)= D @ A(t)"""
-
-    @classmethod
-    def generate_case(cls, eps):
-        n = 100
-        T_1 = cls.get_skew_symmetric(n, [1])
-        T_2 = cls.get_skew_symmetric(n, [0.5, 1])
-        A_1 = cls.get_A(eps=eps)
-        A_2 = cls.get_A(eps=eps)
-
-        A_0 = A_1 + A_2
-
-        def Q_1(t):
-            return sl.expm(T_1 * t)
-
-        def Q_1_dot(t):
-            return T_1 @ Q_1(t)
-
-        def Q_2(t):
-            return sl.expm(T_2 * t)
-
-        def Q_2_dot(t):
-            return T_2 @ Q_2(t)
-
-        def A(t):
-            return Q_1(t) @ (A_1 + np.exp(t) * A_2) @ Q_2(t).T
-
-        def A_dot(t):
-            Q_1_t = Q_1(t)
-            Q_2_t = Q_2(t)
-            A_t = A(t)
-
-            d_one = T_1 @ A_t
-            d_two = A_t @ T_2.T
-            d_tree = Q_1_t @ (np.exp(t) * A_2) @ Q_2_t.T
-
-            return d_one + d_two + d_tree
-
-        return A_0, A, A_dot
-
-    @staticmethod
-    def get_skew_symmetric(n, coeffs):
-        T = np.zeros((n, n))
-        for k, coeff in enumerate(coeffs):
-            i = k + 1
-            np.fill_diagonal(T[i:, :-i], coeff)
-            np.fill_diagonal(T[:-i, i:], -coeff)
-
-        return T
-
-    @staticmethod
-    def get_A(eps=1e-3):
-        A_ = np.eye(10)
-        A_delta = np.random.rand(10, 10) * 0.5
-        A = np.random.rand(100, 100) * eps
-        A[:10, :10] = A_ + A_delta
-        return A
+def get_A(eps):
+    A_ = np.eye(10)
+    A_delta = np.random.rand(10, 10) * 0.5
+    A = np.random.rand(100, 100) * eps
+    A[:10, :10] = A_ + A_delta
+    return A
--- test/test_low_rank_approx.py
+++ test/test_low_rank_approx.py
@@ -9,13 +9,13 @@ from linalg.integrate import matrix_ode_simple
 from linalg.bidiagonalization import lanczos_bidiag_reorth
 from linalg.helpers import make_bidiagonal, multiply_factorized
 
-from test.case_matrix_ode import HeatEquation, FirstExample
+from test.case_matrix_ode import generate_heat_equation, generate_first_example, generate_second_example
 
 
 class TestMatrixOde(unittest.TestCase):
     def testSolutionMap(self):
         print("\nTesting Heat Eqation Case:")
-        A_0, A, A_dot = HeatEquation.generate_case(5, 5, k=3)
+        A_0, A, A_dot = generate_heat_equation(5, 5, k=3)
         np.testing.assert_allclose(A(0), A_0)
 
     def test_integrator_heat(self):
@@ -25,7 +25,7 @@ class TestMatrixOde(unittest.TestCase):
         print(f"\nTesting simple integrator on HeatEqation m={m}, k={k}:")
 
         # generate case and start conditions
-        A_0, A, A_dot = HeatEquation.generate_case(n=m, m=m, k=k)
+        A_0, A, A_dot = generate_heat_equation(n=m, m=m, k=k)
         b = np.random.rand(m)
         U_0, V_0, alpha, beta = lanczos_bidiag_reorth(A_0, k, b)
         S_0 = make_bidiagonal(alpha, beta).toarray()
@@ -51,7 +51,7 @@ class TestMatrixOde(unittest.TestCase):
         print(f"\nTesting simple integrator on first example eps={eps}:")
 
         # generate case and start conditions
-        A_0, A, A_dot = FirstExample.generate_case(eps=eps)
+        A_0, A, A_dot =generate_first_example(eps=eps)
         b = np.random.rand(m)
         U_0, V_0, alpha, beta = lanczos_bidiag_reorth(A_0, k, b)
         S_0 = make_bidiagonal(alpha, beta).toarray()
@@ -72,6 +72,34 @@ class TestMatrixOde(unittest.TestCase):
 
         print("Total: ", A_1_fro, " Residual", fro_diff)
 
+    def test_integrator_eks_2(self):
+        m = 100
+        k = 20
+        t_f = 0.5
+        eps = 1e-1
+        print(f"\nTesting simple integrator on first example eps={eps}:")
+
+        # generate case and start conditions
+        A_0, A, A_dot =generate_second_example(eps=eps)
+        b = np.random.rand(m)
+        U_0, V_0, alpha, beta = lanczos_bidiag_reorth(A_0, k, b)
+        S_0 = make_bidiagonal(alpha, beta).toarray()
+
+        # integrate
+        Y, T = matrix_ode_simple(
+            0, t_f, Y_0=(U_0, S_0, V_0), X=A_dot, TOL=1e-4, verbose=True
+        )
+        Y_mat = multiply_factorized(*Y[-1])
+
+        # measure difference from last matrix
+        A_1 = A(t_f)
+        A_1_fro = np.linalg.norm(A_1, ord="fro")
+        fro_diff = np.linalg.norm(Y_mat - A_1, ord="fro")
+
+        # does not work on this case yet!
+        self.assertLess(fro_diff, A_1_fro)
+
+        print("Total: ", A_1_fro, " Residual", fro_diff)
 
 class TestCayley(unittest.TestCase):
     def test_caylay(self):
